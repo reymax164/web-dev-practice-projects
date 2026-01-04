6a329af21c814878a59e00d1dd84bd01ae48d4ab
Reymax164 <reyesmaxine164@gmail.com>
2026-01-04 17:32:19 +0800

    Home, PokeCare
    Home:
    - Title and description in aside with transition.
    - Projects are now loaded in projects_data.json
    
    PokeCare:
    - Added name input
    - Object creation changes

--- index.js
+++ index.js
@@ -1,43 +1,113 @@
-// load from projects_description.json
+// load from projects_data.json
 async function loadData() {
-    
-    try {
-        const response = await fetch('./projects_data.json');
-        if (!response.ok) throw new Error("Could not fetch data");
-
-        const data = await response.json();
   
-        return Array.isArray(data) ? data : Object.values(data);
-    }
-    catch (error) {
-        console.error(error);
-    }
-      
+  try {
+    const response = await fetch('./projects_data.json');
+    if (!response.ok) throw new Error("Could not fetch data");
+
+    const data = await response.json();
+
+    // converts the data to array then returns it
+    return Array.isArray(data) ? data : Object.values(data);
+  }
+  catch (error) {
+    console.error(error);
+  }
+    
+}
+
+async function loadProjects() {
+  try {
+    const response = await fetch('./projects_data.json');
+    if (!response.ok) throw new Error("Could not fetch projects");
+
+    const data = await response.json();
+    return data;
+  }
+  catch (error) {
+    console.error(error);
+  }
 }
 
 // dispays description on mouse hover
 async function descriptionLoader() {
-    const myData = await loadData();
-    const projectCards = document.querySelectorAll('.projects');
-    const title = document.getElementById('title');
-    const description = document.getElementById('description');
+  const dataset = await loadData();
+  const projectCards = document.querySelectorAll('.projects');
+  const title = document.getElementById('title');
+  const description = document.getElementById('description');
 
-    projectCards.forEach((card, index) => {
-        card.addEventListener("mouseover", () => {
-            const data = myData[index];
-            if (data) {
-                title.textContent = data.title;
-                description.textContent = data.description;
-            }
-        });
-        card.addEventListener("mouseleave", () => {
-            const data = myData[index];
-            if (data) {
-                title.textContent = "Description";
-                description.textContent = "";
-            }
-        });
+  // tracks active timer for the transiions
+  let currentTimeout;
+
+  projectCards.forEach((card, index) => {
+    const data = dataset[index];
+
+    // mouse over
+    card.addEventListener("mouseover", () => {
+      if (!data) return;
+
+      // stops any pending "Home" revert or previous card switch
+      if (currentTimeout) clearTimeout(currentTimeout);
+
+      // fade out
+      title.style.opacity = 0;
+      description.style.opacity = 0;
+
+      // wait 250ms, swap text, fade In
+      currentTimeout = setTimeout(() => {
+        title.textContent = data.title;
+        description.textContent = data.description;
+
+        title.style.opacity = 1;
+        description.style.opacity = 1;
+      }, 200); 
     });
+
+    // mouse leave
+    card.addEventListener("mouseleave", () => {
+      if (!data) return;
+
+      // stops pending card reveal
+      if (currentTimeout) clearTimeout(currentTimeout);
+
+      // fade out
+      title.style.opacity = 0;
+      description.style.opacity = 0;
+
+      // wait for fade out, reset to default, fade in
+      currentTimeout = setTimeout(() => {
+        title.textContent = "Home";
+        description.textContent = ""; 
+
+        // fade in
+        title.style.opacity = 1;
+        description.style.opacity = 1; 
+      }, 200);
+    });
+  });
 }
 
+async function projectsLoader() {
+  const projects = await loadProjects();
+  const main = document.getElementById('home-main');
+  
+  // all projects to single string
+  let projectCards = "";
+
+  // add to string
+  projects.forEach((project) => {
+    projectCards += `
+      <div class="projects">
+        <a href="${project.href}">
+          <p>${project.title}</p>
+        </a>
+      </div>
+    `;
+  });
+
+  // add to DOM
+  main.innerHTML = projectCards; 
+}
+
+projectsLoader();
 descriptionLoader();
--- pokecare/scripts/pokecare.js
+++ pokecare/scripts/pokecare.js
@@ -3,11 +3,33 @@ import PokeAPI from './pokeapi.js';
 
 // html elements
 const playBtn = document.getElementById('play-btn');
-const closeModal = document.getElementById('close-modal');
 const background = document.getElementById('transparent-background');
+
+// choices
 const choiceModal = document.getElementById('choices-modal');
 const choices = document.querySelectorAll('.choices');
 
+// name input
+const inputDiv = document.getElementById('input-div');
+const nameTextbox = document.getElementById('name-textbox');
+const confirmNameBtn = document.getElementById('confirm-name-btn');
+
+// close buttons declarations
+const closeBtns = document.querySelectorAll('.close-btn');
+const closeChoices = closeBtns[0];
+const closeNameInput = closeBtns[1];
+
+// close events
+closeChoices.addEventListener('click', () => {
+    background.style.display = 'none';
+})
+
+closeNameInput.addEventListener('click', () => {
+    inputDiv.style.display = 'none';
+    choiceModal.style.display = 'flex';
+})
+
+// fetch/api
 const api = new PokeAPI();
 
 // fetch sprites
@@ -15,29 +37,45 @@ choices[0].src = await api.getSprite(Treecko.getSpecies);
 choices[1].src = await api.getSprite(Torchic.getSpecies);
 choices[2].src = await api.getSprite(Mudkip.getSpecies);
 
+// ===== main memu =====
+// Play
 playBtn.addEventListener('click', () => {
     background.style.display = 'block';
+    choiceModal.style.display = 'flex';
 });
 
-closeModal.addEventListener('click', () => {
-    background.style.display = 'none';
-})
+let pokemon = null, choice = null, name = null;
+
+function closeChoices_OpenNameInput() {
+  choiceModal.style.display = "none";
+  inputDiv.style.display = 'block';
+}
 
 // choice
-let pokemon, choice, name;
 choices[0].addEventListener('click', () => {
-    choice = 0;
-    pokemon = new Treecko(name);
-    choiceModal.style.display = "none";
+  choice = 0;
+  closeChoices_OpenNameInput();
 });
+
 choices[1].addEventListener('click', () => {
-    choice = 1;
-    pokemon = new Torchic(name);
-    choiceModal.style.display = "none";
+  choice = 1;
+  closeChoices_OpenNameInput();
 });
+
 choices[2].addEventListener('click', () => {
-    choice = 2;
-    pokemon = new Mudkip(name);
-    choiceModal.style.display = "none";
+  choice = 2;
+  closeChoices_OpenNameInput();
+});
+
+// confirm button in name input
+confirmNameBtn.addEventListener('click', () => {
+  // gets the input
+  name = nameTextbox.value;
+  // creates object based on choice and input
+  if(pokemon === null) {
+    if(choice === 0)       pokemon = new Treecko(name);
+    else if (choice === 1) pokemon = new Torchic(name);
+    else if (choice === 2) pokemon = new Mudkip(name);
+  }
 });
 
